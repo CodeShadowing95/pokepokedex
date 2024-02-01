6ad892be03cf19e79aa5608700ba770c8bdf5135
VastoLorde <ksenbonsakura@gmail.com>
2024-02-01 23:32:18 +0100

    UPDATE: Services and environments

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -2,13 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+import { environment } from '../../environments/environment';
+
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
     private apiUrl = 'https://pokedex2.p.rapidapi.com/pokedex/uk';
-    private apiKey = 'd8adc9738emsh581515b396e9e0ep1605cajsn21713ff44a9c';
-    private apiHost = 'pokedex2.p.rapidapi.com'
+    private apiKey = environment.apiKey;
+    private apiHost = 'pokedex2.p.rapidapi.com';
 
   constructor(private http: HttpClient) { }
 
