d7fcc673a6f77ff9491f89e2208b298aeae21272
VastoLorde <ksenbonsakura@gmail.com>
2024-02-02 11:45:46 +0100

    UPDATE: Some changes

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -2,8 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-import { environment } from '../../environments/environment';
-
 @Injectable({
   providedIn: 'root'
 })
