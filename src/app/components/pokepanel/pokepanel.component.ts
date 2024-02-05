536db00a5cccce6be43496fb15adda6eebeba751
VastoLorde <ksenbonsakura@gmail.com>
2024-02-05 12:19:43 +0100

    UPDATE: Handling data filtering

--- src/app/components/pokepanel/pokepanel.component.ts
+++ src/app/components/pokepanel/pokepanel.component.ts
@@ -13,7 +13,7 @@ export class PokepanelComponent implements OnInit {
     filteredPokemons: PokemonProps[] = [];
 
     // Inputs
-    searchParam: string = '';
+    searchParam: boolean = false;
     typeProperty: string = 'all types'
 
     currentPage = 1;
@@ -40,48 +40,29 @@ export class PokepanelComponent implements OnInit {
         });
     }
 
-    // loadFilteredPokemonsByType(): void {
-    //     const startIndex = (this.currentPage - 1) * 10;
-    //     const endIndex = startIndex + 10;
-
-    //     const res = this.filteredDatasByType.slice(0, 100);
-    //     this.pokemons = res;
-    //     this.data = res.slice(startIndex, endIndex);
-    //     this.numberOfPages = Math.ceil(res.length / 10);
-    // }
-
     onPreviousPageClick(): void {
         this.currentPage--;
         this.loadPokemons();
-        // if(this.typeProperty !== 'all types') {
-        //     this.loadFilteredPokemonsByType();
-        // } else {
-        //     this.loadPokemons();
-        // }
     }
  
     onNextPageClick(): void {
         this.currentPage++;
         this.loadPokemons();
-        // if(this.typeProperty !== 'all types') {
-        //     this.loadFilteredPokemonsByType();
-        // } else {
-        //     this.loadPokemons();
-        // }
     }
 
     onPageChange(pageNumber: number): void {
         this.currentPage = pageNumber;
         this.loadPokemons();
-        // if(this.typeProperty !== 'all types') {
-        //     this.loadFilteredPokemonsByType();
-        // } else {
-        //     this.loadPokemons();
-        // }
     }
 
     onSearchChange(searchTerm: string): void {
-        this.filteredDatas = this.filterItemsByName(searchTerm);
+        this.searchParam = (searchTerm !== '');
+        if(searchTerm) {
+            this.filteredDatas = this.filterItemsByName(searchTerm);
+        } else {
+            this.filteredDatas = [];
+            this.loadPokemons();
+        }
     }
 
     onTypeSearch(type: string): void {
