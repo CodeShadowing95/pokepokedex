c2975a34371b9642eb802c1f0a6e5a00e1e50a1e
CodeShadowing95 <ksenbonsakura@gmail.com>
2024-02-11 03:43:49 +0100

    UPDATE: Handling searchbar with datas filtered

--- src/app/components/searchbar/searchbar.component.ts
+++ src/app/components/searchbar/searchbar.component.ts
@@ -1,11 +1,12 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-searchbar',
   templateUrl: './searchbar.component.html',
   styleUrl: './searchbar.component.css'
 })
-export class SearchbarComponent { 
+export class SearchbarComponent {
+  @Input() isDisabled!: boolean;
   @Output() searchChange = new EventEmitter<string>();
 
   onSearchChange(event: Event): void {
