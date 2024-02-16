1082a560ceb6a5fcf1f9c55d1b8faebb14557209
CodeShadowing95 <ksenbonsakura@gmail.com>
2024-02-16 19:43:17 +0100

    FIX: Handling git errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// Sections
 import { HeroComponent } from './components/hero/hero.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavComponent } from './components/nav/nav.component';
 import { PokepanelComponent } from './components/pokepanel/pokepanel.component';
+// Components
 import { PokecardComponent } from './components/pokecard/pokecard.component';
 import { PokedetailsComponent } from './components/pokedetails/pokedetails.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
@@ -17,17 +23,16 @@ import { DropdownFilterComponent } from './components/dropdown-filter/dropdown-f
 import { DialogBoxComponent } from './components/dialog-box/dialog-box.component';
 import { LikeComponent } from './components/like/like.component';
 import { DialogCatchComponent } from './components/dialog-catch/dialog-catch.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { NavComponent } from './components/nav/nav.component';
 import { LanguageComponent } from './components/language/language.component';
 import { ToTopButtonComponent } from './components/to-top-button/to-top-button.component';
 import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { HintComponent } from './components/hint/hint.component';
+import { DialogHintComponent } from './components/dialog-hint/dialog-hint.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroComponent,
-    PokepanelComponent,
     PokecardComponent,
     PokedetailsComponent,
     PaginationComponent,
@@ -43,6 +48,9 @@ import { PortfolioComponent } from './components/portfolio/portfolio.component';
     LanguageComponent,
     ToTopButtonComponent,
     PortfolioComponent,
+    PokepanelComponent,
+    HintComponent,
+    DialogHintComponent,
   ],
   imports: [
     BrowserModule,
